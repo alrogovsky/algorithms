dd424de89876f0ffc21ed302c60d41b78a21085f
alex-leet <alex@rogovsky.ru>
2015-03-10 23:59:08 +0300

    fixed

--- task4/main.cpp
+++ task4/main.cpp
@@ -5,7 +5,7 @@
 
 class CQueue {
 public:
-    CQueue( int size );
+    CQueue(int size);
     ~CQueue() { delete[] buffer; }
 
     //Функции
@@ -13,7 +13,10 @@ public:
     int Dequeue();
 
     //Проверка на пустоту
-    bool IsEmpty() { return head==tail; }
+    bool IsEmpty() { return head == tail; }
+
+    //Принт для тестов
+    void print();
 
 private:
     int* buffer;
@@ -25,44 +28,119 @@ private:
     void grow();
 };
 
-CQueue::CQueue( int size ) : bufferSize(size), head(0), tail(0)
+//Конструктор
+CQueue::CQueue(int size) : bufferSize(size), head(0), tail(0)
 {
     buffer = new int[bufferSize];
 }
 
+//Вход в очередь
 void CQueue::Enqueue(int a)
 {
-    if( ( tail + 1 ) % bufferSize == head)
+    if ((tail + 1) % bufferSize == head)
         grow();
 
     buffer[tail] = a;
-    ++tail;
-    std::cout<<tail;
+    tail = (tail + 1) % bufferSize;
 }
 
+//Выход из очереди
 int CQueue::Dequeue()
 {
-    assert( head != tail );
-    int result = buffer[head];
-    head = (head+1) % bufferSize;
-    return result;
+    if (!IsEmpty()) {
+        int result = buffer[head];
+        head = (head + 1) % bufferSize;
+        return result;
+    }
+    else {
+        return -1;
+    }
 }
 
+//Реаллокация
 void CQueue::grow()
 {
-    std::cout<<"POPAL!";
+    //std::cout << "i'm in grow!!!\n\n";
     int newSize = bufferSize * GROW_RATE;
     int* new_buffer = new int[newSize];
+    int newBufferSize = bufferSize * GROW_RATE;
+    if (head < tail) {
+        for (int i = 0; i < tail; i++) {
+            new_buffer[i] = buffer[i];
+        }
+    }
+    else {
+        int current = 0;
+        for (int i = 0; i < bufferSize - head; i++) {
+            new_buffer[current] = buffer[head + i]; \
+                current++;
+        }
+        for (int i = 0; i < tail; i++) {
+            new_buffer[current] = buffer[i];
+            current++;
+        }
+        head = 0;
+        tail = current;
+    }
 
+    delete[] buffer;
 
+    buffer = new_buffer;
+    bufferSize = newBufferSize;
+
+}
+
+//Принт для тестов
+void CQueue::print()
+{
+    std::cout << "printing for BUFF SIZE: " << bufferSize << " ///// head = " << head << " ///// tail = " << tail << "\n";
+    for (int i = 0; i < bufferSize; i++)
+    {
+        std::cout << buffer[i] << " ";
+    }
+
+    std::cout << "\n\n\n";
+}
+
+bool IsPossible(CQueue &Q) {
+    int n;
+    std::cin >> n;
+    if(n == 0) return false;
+
+    bool answer = true;
+    for (int i = 0; i < n; i++) {
+        int command, value;
+        std::cin >> command >> value;
+        assert( command == 3 || command == 2);
+        switch (command) {
+            default: answer = false;
+            case 2: {
+                        int get = Q.Dequeue();
+                        answer = answer && (value == get);
+                        break;
+            }
+            case 3: {
+                        Q.Enqueue(value);
+                        break;
+            }
+
+        }
+       // Q.print();
+    }
+
+    return answer;
 }
 
 int main()
 {
-    CQueue Q(2);
-    Q.Enqueue(10);
-    Q.Enqueue(5);
-    Q.Enqueue(44);
+    CQueue Q(4);
+
+    if (IsPossible(Q)) {
+        std::cout << "YES";
+    }
+    else {
+        std::cout << "NO";
+    }
+
     return 0;
 }
-
